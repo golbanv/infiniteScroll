5338688e9ed3e3ca1085f5e9f61a6f41ce12c698
golbanv <golbanvladislav@gmail.com>
2020-08-07 13:08:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArray = [];
+
+const count = 10;
+const apiKey = "1zcphoL1BvMs3op8nD4WUR04hDktyEFLG1u4j6jZzk8";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+function displayPhotos(){
+    photosArray.forEach((photo) => {
+
+        const item = document.createElement("a");
+        setAttributes(item,{
+            href: photo.links.html,
+            target:"_blank",
+        })
+
+        const img = document.createElement("img");
+        setAttributes(img,{
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    });
+}
+
+async function getPhotos(){
+
+    try {
+        const response = await  fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        console.log("Whoops", error);
+    }
+}
+
+getPhotos();
